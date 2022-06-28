4cba7e3c2acbe18694365bc13f7786d853f003e1
H Suhas Prabhu <85097320+Suhas-2002@users.noreply.github.com>
2022-06-28 09:55:24 +0530

    Create optimized_bubble_sort.java

---
+++ JAVA/Sorting/optimized_bubble_sort.java
@@ -0,0 +1,40 @@
+package Sorting;
+import java.util.Scanner;
+
+public class optimized_bubble_sort{
+
+	public static void sort(int[] a) {
+		for (int i = 0; i < a.length-1; i++) {
+      boolean flag = false;
+			for (int j = 0; j < a.length-i-1; j++) {
+				if (a[j] > a[j + 1]) {
+					int t = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = t;
+          flag = true;
+				}
+			}
+      if(!flag) break;
+		}
+	}
+
+	public static void main(String[]args) {
+		int n=10;
+		int[] a = new int[n];
+		System.out.println("--------------------Bubble Sort--------------------");
+		Scanner s = new Scanner(System.in);
+		for (int i = 0; i < n; i++) {
+			a[i] = s.nextInt();
+		}
+		System.out.print("Before Sorting : ");
+		for (int i:a) {
+			System.out.print(i+" ");
+		}
+		sort(a);
+		System.out.println("");
+		System.out.print("After Sorting : ");
+		for (int i:a) {
+			System.out.print(i+" ");
+		}
+	}
+}
